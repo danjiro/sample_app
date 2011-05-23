4e2dc84532e40aecab187cdebf2445f24d840e9c
Danny Tan <dannytan11@gmail.com>
2011-05-23 16:46:32 +0900

    added user microposts

--- Gemfile
+++ Gemfile
@@ -3,11 +3,14 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.7'
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
+  gem 'rake', '0.8.7'
   gem 'rspec-rails', '2.5.0'
   gem 'webrat'
   gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
@@ -15,5 +18,6 @@ group :test do
   gem 'spork', '0.9.0.rc5'
   gem 'factory_girl_rails', '1.0'
 end
+
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,47 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
+    def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+    end
+    
+     def destroy
+       if User.find(params[:id]).admin?
+         flash[:error] = "Cannot delete Admins!"
+             redirect_to users_path
+       else
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+       end 
+  end
+    
+
+    
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
   
   def new
+    if signed_in?
+      redirect_to(root_path)
+    else
     @title = "Sign up"
     @user = User.new
+    end
   end
+  
   
   def create
     @user = User.new(params[:user])
+    if signed_in?
+      redirect_to(root_path)
+    end
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -25,5 +55,33 @@ class UsersController < ApplicationController
       
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+
+private
+
+
+def correct_user
+  @user = User.find(params[:id])
+   redirect_to(root_path) unless current_user?(@user)
+end
+
+ def admin_user
+      redirect_to(root_path) unless current_user.admin?
+ end 
 
 end
